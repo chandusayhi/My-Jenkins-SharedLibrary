6008f47cbd7be6ad7d67da09681949c0f31479b3
Chandrakant Hatti <chandusayhi@users.noreply.github.com>
2024-04-16 18:58:57 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,16 +4,3 @@ def java(){
       mvn clean package
     '''
 }
-
-def react(){
-  echo "Building React Application"
-  bat """
-    cd /d F:
-    cd F:\\${appName}\\
-    npm run build
-  """
-
-    bat """
-        powershell Compress-Archive -Path "F:\\${appName}\\build\\*" -DestinationPath "F:\\${appName}_${BUILD_NUMBER}.zip"
-    """
-}
